a26107498b59d2cc1496eede5adc621111b18449
t <82817414+Derek46518@users.noreply.github.com>
2023-04-25 19:51:24 +0800

    Update Multiplier

--- Multiplier.v
+++ Multiplier.v
@@ -5,19 +5,32 @@ input clk, reset;
 input [31:0] dataA, dataB;
 input [5:0] Signal;
 output[63:0] dataOut;
-reg[63:0] Product;
+reg[63:0] Product,temp;
 
 always@(Signal)
     begin
         if (Signal == MULTU) 
         begin
-            Product = {32'b0, dataB};
+            Product = 64'b0;
+            temp = {32'b0,dataA[31:0]}
         end
     end
     always@(posedge clk or reset)
     begin
-        if(Signal == MULTU)
-        
+        if(Signal == MULTU) 
+        begin
+            if(dataB[0]) begin
+                Product = temp+dataB;
+            end
+
+            dataB = dataB >> 1;
+            temp = temp << 1;
+        end
+        else
+        begin
+        if(reset) begin
+            Product <=64'b0;
+            temp <= {32'b0,dataA[31:0]};
         end
     end
 end
